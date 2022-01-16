3def547933a6dc011d145727980f6d378c1e4c63
koutyan <develop@kosukelab.com>
2022-01-16 23:12:44 +0900

    Fix config file.

--- bot_config.py
+++ bot_config.py
@@ -2,8 +2,8 @@
 # This is config file NOT main program.
 
 # Enter your Discord token and channel id.
-DISCORD_TOKEN = ''
-DISCORD_CHANNEL_ID = 0000000000000000
+DISCORD_TOKEN=''
+DISCORD_CHANNEL_ID=0000000000000000
 
 # Enter your minecraft container id.
-CONTAINER_ID = ''
+CONTAINER_ID=''
